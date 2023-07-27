36564985246816a089975805418ee03a2a1dbd51
Albert4940 <dorcealbertmary@gmail.com>
2023-07-27 16:22:15 -0400

    Setup server

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Starting");
+const dotenv = require('dotenv').config();
+const express = require('express');
+var path = require('path');
+let app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req,res)=>{
+ res.sendFile(__dirname + '/views/index.html');
+})
+
+
+app.listen(port);
